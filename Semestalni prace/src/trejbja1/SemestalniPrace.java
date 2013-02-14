fc4eadf794b1ce79ffda063d212eb7597bfd2062
Jan Trejbal <jan.trejbal@gmail.com>
2013-02-14 15:27:01 +0100

    Prvni commit - vytvoreni projektu

---
+++ Semestalni prace/src/trejbja1/SemestalniPrace.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trejbja1;
+
+/**
+ *
+ * @author Jan
+ */
+public class SemestalniPrace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
