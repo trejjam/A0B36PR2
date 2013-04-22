887d9acf43fac140e89436ce615a8d328fba5077
Jan Trejbal <jan.trejbal@gmail.com>
2013-04-22 11:24:51 +0200

    edit: add loading label from language file

--- Semestalni prace/src/trejbja1/App.java
+++ Semestalni prace/src/trejbja1/App.java
@@ -4,6 +4,7 @@
  */
 package trejbja1;
 
+import java.util.Map;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 
@@ -12,35 +13,69 @@ import javax.swing.ImageIcon;
  * @author Jan
  */
 public class App extends javax.swing.JFrame {
-  private BridgeAppCode bridge;
-  private DefaultComboBoxModel IPmodel = new javax.swing.DefaultComboBoxModel(new String[] {});
-  private String sLastIp="";
-  private String sPort="";
-  
-  
-  public void setLastIp(String sLastIp) {
-      this.sLastIp=sLastIp;
-  }
-  
-  public void setPort(String sPort) {
-      this.sPort=sPort;
-  }
-  
-  public void setPort(DefaultComboBoxModel IPmodel) {
-      this.IPmodel=IPmodel;
-  }
-  
-  public DefaultComboBoxModel getIpModul() {
-      return IPmodel;
-  }
-  
-  public String getIP() {
-      return ipAddress.getSelectedItem().toString();
-  }
-  
-  public int getPort() {
-      return Integer.parseInt(port.getText());
-  }
+    private BridgeAppCode bridge;
+    private DefaultComboBoxModel IPmodel = new javax.swing.DefaultComboBoxModel(new String[] {});
+    private DefaultComboBoxModel LangModel = new javax.swing.DefaultComboBoxModel(new String[] {});
+    private String sLastIp="";
+    private String sPort="";
+    private String sLang="";
+    private Map<String, String> langValues;
+
+    public App() {
+        setIcon();
+        bridgeInit();
+        initComponents();
+        setVisible(true);   
+    }
+    
+    public void setLangValues(Map<String, String> langValues) {
+        this.langValues = langValues;
+    }
+
+    public void setLastIp(String sLastIp) {
+        this.sLastIp=sLastIp;
+    }
+
+    public void setPort(String sPort) {
+        this.sPort=sPort;
+    }
+    
+    public void setLang(String sLang) {
+        this.sLang=sLang;
+    }
+
+    public void setPort(DefaultComboBoxModel IPmodel) {
+        this.IPmodel=IPmodel;
+    }
+
+    public DefaultComboBoxModel getIpModul() {
+        return IPmodel;
+    }
+    
+    public DefaultComboBoxModel getLangModul() {
+        return LangModel;
+    }
+
+    public String getIP() {
+        return ipAddress.getSelectedItem().toString();
+    }
+
+    public int getPort() {
+        return Integer.parseInt(port.getText());
+    }
+
+    private void bridgeInit() {
+        if (bridge==null) {
+            bridge = new BridgeAppCode(this);
+            langValues=bridge.getLangValues();
+        }
+    }
+
+    private void setIcon() {
+        ImageIcon img = new ImageIcon(getClass().getResource("/resources/ship.png"));
+
+        this.setIconImage(img.getImage());
+    }
 
   /**
    * This method is called from within the constructor to initialize the form.
@@ -55,18 +90,21 @@ public class App extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         conn = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         ipAddress = new javax.swing.JComboBox();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         port = new javax.swing.JFormattedTextField();
         save = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        ipAddress1 = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jTabbedPane1.setName("LBoat"); // NOI18N
 
-        conn.setText("Připojit");
+        conn.setText(langValues.get("Connect"));
         conn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 connActionPerformed(evt);
@@ -80,6 +118,13 @@ public class App extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("rCam");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -89,7 +134,9 @@ public class App extends javax.swing.JFrame {
                 .addComponent(conn)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addContainerGap(516, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton2)
+                .addContainerGap(415, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -97,13 +144,14 @@ public class App extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(conn)
-                    .addComponent(jButton1))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2))
                 .addContainerGap(321, Short.MAX_VALUE))
         );
 
         conn.getAccessibleContext().setAccessibleName("conn");
 
-        jTabbedPane1.addTab("Ovládání", jPanel1);
+        jTabbedPane1.addTab(langValues.get("mainLabel"), jPanel1);
         jPanel1.getAccessibleContext().setAccessibleName("control");
 
         ipAddress.setEditable(true);
@@ -111,19 +159,26 @@ public class App extends javax.swing.JFrame {
         ipAddress.setSelectedItem(sLastIp);
         ipAddress.setToolTipText("");
 
-        jLabel1.setText("IP adresa");
+        jLabel1.setText(langValues.get("IpAddress"));
 
-        jLabel2.setText("Port");
+        jLabel2.setText(langValues.get("Port"));
 
         port.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0"))));
         port.setText(sPort);
 
-        save.setText("Uložit");
+        save.setText(langValues.get("Save"));
         save.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 saveActionPerformed(evt);
             }
         });
+
+        jLabel3.setText(langValues.get("Language"));
+
+        ipAddress1.setEditable(true);
+        ipAddress1.setModel(LangModel);
+        ipAddress1.setSelectedItem(sLang);
+        ipAddress1.setToolTipText("");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -132,15 +187,20 @@ public class App extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(save)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(ipAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)
+                                .addComponent(ipAddress1, 0, 1, Short.MAX_VALUE))
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel1)
+                                .addComponent(ipAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(port, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2)))
-                    .addComponent(save))
+                            .addComponent(jLabel2))))
                 .addContainerGap(280, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
@@ -155,14 +215,19 @@ public class App extends javax.swing.JFrame {
                     .addComponent(ipAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(port, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(ipAddress1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(save)
-                .addContainerGap(270, Short.MAX_VALUE))
+                .addContainerGap(239, Short.MAX_VALUE))
         );
 
         ipAddress.getAccessibleContext().setAccessibleName("ipBox");
         save.getAccessibleContext().setAccessibleName("save");
+        ipAddress1.getAccessibleContext().setAccessibleName("LangBox");
 
-        jTabbedPane1.addTab("Nastavení připojení", jPanel2);
+        jTabbedPane1.addTab(langValues.get("ConnectSettings"), jPanel2);
         jPanel2.getAccessibleContext().setAccessibleName("settings");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -181,19 +246,6 @@ public class App extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-  private void bridgeInit() {
-    if (bridge==null) {
-      bridge = new BridgeAppCode(this);
-      setIcon();
-    }
-  }
-  
-  private void setIcon() {
-    ImageIcon img = new ImageIcon(getClass().getResource("/resources/ship.png"));
-    
-    this.setIconImage(img.getImage());
-  }
   
   private void connActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connActionPerformed
     // TODO add your handling code here:
@@ -211,19 +263,10 @@ public class App extends javax.swing.JFrame {
         bridge.getPhoto();
     }//GEN-LAST:event_jButton1ActionPerformed
 
-  
-  public void init() {
-    /* Create and display the form */
-    java.awt.EventQueue.invokeLater(new Runnable() {
-        @Override
-        public void run() {
-            App app=new App();
-            app.bridgeInit();
-            app.initComponents();
-            app.setVisible(true);
-        }
-    });
-  }
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        bridge.rCam();
+    }//GEN-LAST:event_jButton2ActionPerformed
   
 /*  public void redesign() {
     try {
@@ -245,9 +288,12 @@ public class App extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton conn;
     public javax.swing.JComboBox ipAddress;
+    public javax.swing.JComboBox ipAddress1;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     public javax.swing.JPanel jPanel2;
     private javax.swing.JTabbedPane jTabbedPane1;
--- Semestalni prace/src/trejbja1/BridgeAppCode.java
+++ Semestalni prace/src/trejbja1/BridgeAppCode.java
@@ -4,13 +4,16 @@
  */
 package trejbja1;
 
-import java.util.Arrays;
-import java.util.Vector;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.ComboBoxModel;
-import javax.swing.DefaultComboBoxModel;
+import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -20,63 +23,160 @@ public class BridgeAppCode {
     private TcpIp conn= null;
     private App app;
     private String configFile="config.sys";
+    private int connDelay;
+    private Timer connTimer=null;
     
-    Xml xml=new Xml(configFile);
+    
+    //language map
+    private Map<String, String> langValues;
+    //element status map
+    private Map<String, Integer> elementStat;
+    
+    Xml xml;
+    Xml xmlLang;
 
     public BridgeAppCode(App app) { //init
         System.out.println("create bridge");
+        
+        xml=new Xml(configFile);
+        langValues = new HashMap<>();
+        
+        elementStat = new HashMap<>();
+        initElementStat();
+         
         this.app=app;
         
         loadConfig();
+        loadLanguageValues();
     }
-
     public void connButton() {
-        if (conn==null) {
-            conn=new TcpIp(app.getIP(), app.getPort());
-            app.conn.setText("Odpojit");
+        if (conn==null || elementStat.get("conn")==0) {
+            conn=new TcpIp(app.getIP(), app.getPort(), this);
+            
+            connDelay=conn.getConDelay()/1000;
+            setConnButon(langValues.get("Connecting")+" ("+ (connDelay+1) +")", -1);
+            
+            connTimer = new Timer();
+            connTimer.scheduleAtFixedRate(new TimerTask() {
+                
+                @Override
+                public void run() {
+                  setConnButon(langValues.get("Connecting")+" ("+ (connDelay--) +")", -1);
+                  
+                  if (connDelay==0) {
+                      this.cancel();
+                  }
+                }
+            }, 0, 1000);
+        }
+        else if (elementStat.get("conn")==-1) {                
+            conn.close();
+            conn=null;
+            
+            setConnButon(langValues.get("Connect"), 0);
         }
         else {
             conn.close();
             conn=null;
-            app.conn.setText("Připojit");  
+            setConnButon(langValues.get("Connect"), 0);  
         }
         System.out.println("connButton");
     }
-
+    public void stopConnTimer() {
+        if (connTimer==null) return;
+        connTimer.cancel();
+    }
+    synchronized public void setConnButon(String text, int statutConn) {
+        app.conn.setText(text);
+        elementStat.put("conn", statutConn);
+    }
     public void saveSettings() {
         System.out.println("save");
     }
-
     public void getPhoto() {
         System.out.println("photo");
         conn.send(new String(new char[] {0x56, 0, 0x36, 0x01, 0}));
     }
-  
+    public void rCam() {
+        System.out.println("rCam");
+        conn.send(new String(new char[] {0x56, 0, 0x26, 0}));
+    }
     private void loadConfig() {
         if (!xml.Load()) return;
         
         //load IP
         int IPsCount;
+        try {
+            langValues.put("Language", xml.getValueThrows("Config", "Language"));
+        } catch (XPathExpressionException ex) {
+            langValues.put("Language", "en");
+        }
         
         try {
-            IPsCount = Integer.parseInt(xml.getKey("/localizableStrings/group[@id='Config']/text[@id='IpCount']"));
-            ComboBoxModel model = app.ipAddress.getModel();
-            
-            for (int i=1; i<=IPsCount; i++) {
-                app.getIpModul().addElement(xml.getKey("/localizableStrings/group[@id='Config']/text[@id='IP_"+i+"']"));
-            }
-            
-            app.setLastIp(xml.getKey("/localizableStrings/group[@id='Config']/text[@id='LastIp']"));
+            xmlLang = new Xml(xml.getValueThrows("Config", "LanguageFile"), langValues.get("Language"));
         } catch (XPathExpressionException ex) {
-            Logger.getLogger(SemestalniPrace.class.getName()).log(Level.SEVERE, null, ex);
+            File is = new File("language.xml");
+            if (is.exists()) xmlLang = new Xml("language.xml", langValues.get("Language"));
         }
         
-        //load port
-        try {            
-            app.setPort(xml.getKey("/localizableStrings/group[@id='Config']/text[@id='Port']"));
-            //app.ipAddress.addItem(IPs);
+        if (xmlLang!=null) xmlLang.Load();
+        
+        try {
+            IPsCount = Integer.parseInt(xml.getValueThrows("Config", "IpCount"));
         } catch (XPathExpressionException ex) {
-            Logger.getLogger(SemestalniPrace.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("IPscount not loaded");
+            IPsCount=0;
         }
+
+        for (int i=1; i<=IPsCount; i++) {
+            try {
+                String IP = xml.getValueThrows("Config", "IP_"+i);
+                app.getIpModul().addElement(IP);
+            } catch (XPathExpressionException ex) {
+                Logger.getLogger(BridgeAppCode.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        app.setLastIp(xml.getValue("Config", "LastIp"));
+        
+        //load port          
+        app.setPort(xml.getValue("Config", "Port"));
+    }
+    private void initElementStat() {
+        elementStat.put("conn", 0);
+    }
+    private void loadLanguageValues() {
+        if (xmlLang==null) return;
+        
+        langValues.put("LanguageName", xmlLang.getLanguageValue("Lang", "LanguageName"));
+        
+        langValues.put("mainLabel", xmlLang.getLanguageValue("main", "CardName"));
+        langValues.put("Connect", xmlLang.getLanguageValue("main", "Connect"));
+        langValues.put("Connecting", xmlLang.getLanguageValue("main", "Connecting"));
+        langValues.put("Disconnect", xmlLang.getLanguageValue("main", "Disconnect"));
+        
+        langValues.put("ConnectSettings", xmlLang.getLanguageValue("settings", "ConnectSettings"));
+        langValues.put("IpAddress", xmlLang.getLanguageValue("settings", "IpAddress"));
+        langValues.put("Port", xmlLang.getLanguageValue("settings", "Port"));
+        langValues.put("WifiNetwork", xmlLang.getLanguageValue("settings", "WifiNetwork"));
+        langValues.put("WifiPassword", xmlLang.getLanguageValue("settings", "WifiPassword"));
+        langValues.put("Language", xmlLang.getLanguageValue("settings", "Language"));
+        langValues.put("Save", xmlLang.getLanguageValue("settings", "Save"));
+        
+        try {
+            String langs = xmlLang.getByKey("/localizableStrings/group[@id='Lang']/string[@id='LanguageName']");
+            String[] langsArr = langs.split("\n");
+            for (int i = 1; i < langsArr.length-1; i++) {
+                String lang = langsArr[i].trim();
+                app.getLangModul().addElement(lang);
+            }
+            app.setLang(langValues.get("LanguageName"));
+        } catch (XPathExpressionException ex) {
+            Logger.getLogger(BridgeAppCode.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Map<String, String> getLangValues() {
+        return langValues;
     }
 }
